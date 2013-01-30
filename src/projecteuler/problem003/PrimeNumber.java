3d4c218e24caa17823f99226d6d2052e7f688e8e
Bruno Bonamici <bruno.bonamici@gmail.com>
2013-01-30 12:21:36 -0300

    updates

--- src/projecteuler/problem003/PrimeNumber.java
+++ src/projecteuler/problem003/PrimeNumber.java
@@ -1,4 +1,4 @@
-package bonamici.math;
+package projecteuler.problem003;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,20 +11,6 @@ public class PrimeNumber {
 		}
 	}
 
-	// def trial_division(n):
-	// """Return a list of the prime factors for a natural number."""
-	// if n == 1: return [1]
-	// primes = prime_sieve(int(n**0.5) + 1)
-	// prime_factors = []
-	//
-	// for p in primes:
-	// if p*p > n: break
-	// while n % p == 0:
-	// prime_factors.append(p)
-	// n //= p
-	// if n > 1: prime_factors.append(n)
-	//
-	// return prime_factors
 
 	public static List<Long> getPrimeFactors(long number) {
 		List<Long> primeFactorList = new ArrayList<Long>();
